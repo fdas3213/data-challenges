d1fa1b28024dba5d8cd7ea29013a548d07aa89ed
fdas3213 <szx9404@gmail.com>
2018-07-23 13:48:26 -0700

    spanish translation finished

--- helper_f.py
+++ helper_f.py
@@ -7,19 +7,39 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 import seaborn as sns
 
-def plot_freq(columns, label, df, ylabel):
+def plot_freq(columns, label, df, ylabel, rotation = False):
 	for column in columns:
 		freq_table = pd.crosstab(index = df[column], columns = df[label], normalize = 'index')
 		plt.figure()
 		plt.bar(x = freq_table.index, height = freq_table[1])
 		plt.xlabel(column)
 		plt.ylabel(ylabel)
+		if rotation:
+			plt.xticks(rotation = 45)
 
 def plot_continuous(columns, label, df):
 	for column in columns:
 		print(column)
 		hist = df[[column, label]].hist(by = label, bins = 30)
 		plt.show()
+
+def plot_feature_importance(df, label, clf, degree):
+	feature_columns = df.loc[:, df.columns != label].columns.values
+	imp_df = pd.DataFrame({"features":feature_columns, "importance": clf.feature_importances_})
+	imp_df = imp_df.sort_values('importance').reset_index(drop = True)
+	plt.title("Feature importance")
+	plt.bar(x = imp_df['features'], height = imp_df['importance'])
+	plt.xticks(rotation = degree)
+
+def t_test(df, group_label, label):
+	group_a = df[df[group_label] == 0][label]
+	group_b = df[df[group_label] == 1][label]
+	#calculate mean and std
+	var_a = group_a.var(ddof = 1)
+	var_b = group_b.var(ddof = 1)
+	denom = np.sqrt((var_a/len(group_a)) + (var_b/len(group_b)))
+	t_score = (group_a.mean() - group_b.mean()) / denom
+	return t_score
 
 def plot_classifier(X,y):
 	classifiers = [RandomForestClassifier(), LogisticRegression()]
