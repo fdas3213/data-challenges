ce44be715e39af78ab87e1979167ed22532b61f1
fdas3213 <szx9404@gmail.com>
2018-08-02 16:51:08 -0700

    updates on fraud activity

--- helper_f.py
+++ helper_f.py
@@ -3,32 +3,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import accuracy_score, log_loss
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 import seaborn as sns
+from datetime import datetime
 
+"""use this function to visualize the relationship between
+a categorical variable and the target variable. Check out distribution of 
+one class of categorical variable vs. target variable
+"""
 def plot_freq(columns, label, df, ylabel, rotation = False):
 	for column in columns:
 		freq_table = pd.crosstab(index = df[column], columns = df[label], normalize = 'index')
 		plt.figure()
-		plt.bar(x = freq_table.index, height = freq_table[1])
+		freq_table.plot.bar()
 		plt.xlabel(column)
 		plt.ylabel(ylabel)
 		if rotation:
 			plt.xticks(rotation = 45)
 
+"""use this function to visualize continuous variable vs.
+one class of target variable
+"""
 def plot_continuous(columns, label, df):
 	for column in columns:
 		print(column)
+		df[column].hist(bins = 30)
 		hist = df[[column, label]].hist(by = label, bins = 30)
 		plt.show()
 
+"""
+check out mean value of a continuous variable vs. one class of 
+target variable
+"""
 def plot_mean_value(columns, label, df):
 	for column in columns:
 		plt.figure()
 		new_df = df.groupby(label)[column].mean()
 		new_df.plot.bar()
 		plt.ylabel("average " + column)
+        
+"""
+user histogram to plot frequency of target varibale
+"""
+def plot_target(label, df):
+	df[label].hist()
+	plt.xlabel(label)
+	plt.ylabel('frequency')
+
 
 def plot_feature_importance(df, label, clf, degree):
 	feature_columns = df.loc[:, df.columns != label].columns.values
@@ -37,6 +59,7 @@ def plot_feature_importance(df, label, clf, degree):
 	plt.title("Feature importance")
 	plt.bar(x = imp_df['features'], height = imp_df['importance'])
 	plt.xticks(rotation = degree)
+    
 
 def t_test(df, group_label, label):
 	group_a = df[df[group_label] == 0][label]
@@ -48,8 +71,9 @@ def t_test(df, group_label, label):
 	t_score = (group_a.mean() - group_b.mean()) / denom
 	return t_score
 
+"""consider adding SVM and GB to this list"""
 def plot_classifier(X,y):
-	classifiers = [RandomForestClassifier(), LogisticRegression()]
+	classifiers = [RandomForestClassifier(), LogisticRegression(), GradientBoostingClassifier()]
 
 	log_cols = ['Classifier', 'Accuracy']
 	log = pd.DataFrame(columns = log_cols)
@@ -75,7 +99,7 @@ def plot_classifier(X,y):
 
 	print(log)
 
-	plt.xlbale('Accuracy')
+	plt.xlabel('Accuracy')
 	plt.title('Classifier Accuracy')
 
 	sns.set_color_codes('muted')
